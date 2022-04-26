bb7e515accdcb2f853098fadd1a9f04094f09585
PearlDS <62283615+PearlDS@users.noreply.github.com>
2022-04-26 14:33:40 +0200

    Update Cornivore.java

--- src/be/intecbrussel/notebook/entities/animals/Cornivore.java
+++ src/be/intecbrussel/notebook/entities/animals/Cornivore.java
@@ -2,7 +2,31 @@ package be.intecbrussel.notebook.entities.animals;
 
 public class Cornivore extends Animal{
 
+    //1. variabelen
     private double maxFoodSize;
+    
+    //2.Constructors
+    
+    
+    public Cornivore(String name) {
+        super(name);
+    }
+
+    public Cornivore(String name, double weight, double height, double length) {
+        super(name, weight, height, length);
+    }
+    
+    //3. getters and setters
+    public double getMaxFoodSize() {
+        return maxFoodSize;
+    }
+
+    public void setMaxFoodSize(double maxFoodSize) {
+        this.maxFoodSize = maxFoodSize;
+    }
+    
+    //4.other methods
+    
 
     @Override
     public String toString() {
@@ -15,19 +39,6 @@ public class Cornivore extends Animal{
                 '}';
     }
 
-    public double getMaxFoodSize() {
-        return maxFoodSize;
-    }
+    
 
-    public void setMaxFoodSize(double maxFoodSize) {
-        this.maxFoodSize = maxFoodSize;
-    }
-
-    public Cornivore(String name) {
-        super(name);
-    }
-
-    public Cornivore(String name, double weight, double height, double length) {
-        super(name, weight, height, length);
-    }
 }
